0badd64683440b3b8c19626685054b71e68556e3
MuhammadNurulloh <dodgechalanger9@gmail.com>
2024-09-15 15:59:29 +0300

    Cofeeroasters

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const burgerButtonBtn = document.querySelector(".burger-button");
+const hiddenNavEl = document.querySelector(".hidden-nav");
+const burgerMenuEl = document.querySelector(".burger-menu");
+
+burgerButtonBtn.addEventListener("click", () => {
+  hiddenNavEl.classList.toggle("show-nav");
+  document.body.classList.toggle("overflow-toggle");
+  if (document.body.classList.contains("overflow-toggle")) {
+    burgerMenuEl.src = "./img/shared/mobile/icon-close.svg";
+  } else {
+    burgerMenuEl.src = "./img/shared/mobile/icon-hamburger.svg";
+  }
+});
